fd515fdd564c6961291a09af117e5f3560bbc67b
SuelenLaffinWarren <113142506+SuelenLaffinWarren@users.noreply.github.com>
2022-11-22 10:26:39 -0300

    Update ApiEverest/CrudApi/Services/ICustomerService.cs
    Co-authored-by: Gregory Zimmer <113135420+gregoryzimmerwarren@users.noreply.github.com>

--- ApiEverest/CrudApi/Services/ICustomerService.cs
+++ ApiEverest/CrudApi/Services/ICustomerService.cs
@@ -7,7 +7,6 @@ namespace CustomerApi.Services
         void Create(CustomerEntity model);
         bool Update(CustomerEntity model);
         bool Delete(long id);
-
         IEnumerable<CustomerEntity> GetAll();
 
         CustomerEntity? GetById(long id);
