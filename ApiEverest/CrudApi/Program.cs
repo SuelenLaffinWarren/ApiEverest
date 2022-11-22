b221700dce47a42602dcf04d29634ccc66575fcb
SuelenLaffinWarren <113142506+SuelenLaffinWarren@users.noreply.github.com>
2022-11-22 10:19:46 -0300

    Update ApiEverest/CrudApi/Program.cs
    Co-authored-by: Gregory Zimmer <113135420+gregoryzimmerwarren@users.noreply.github.com>

--- ApiEverest/CrudApi/Program.cs
+++ ApiEverest/CrudApi/Program.cs
@@ -6,7 +6,6 @@ using FluentValidation.AspNetCore;
 
         var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
 
         builder.Services.AddControllers();
         builder.Services.AddFluentValidationAutoValidation();
