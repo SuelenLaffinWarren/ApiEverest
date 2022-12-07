7a1b75c884fe4bb705f9f25ba31921b154c2242c
SuelenLaffinWarren <113142506+SuelenLaffinWarren@users.noreply.github.com>
2022-12-07 09:28:23 -0300

    Update ApiEverest/AppServices/Validators/CustomerCreateValidator.cs
    Co-authored-by: Gregory Zimmer <113135420+gregoryzimmerwarren@users.noreply.github.com>

--- ApiEverest/AppServices/Validators/CustomerCreateValidator.cs
+++ ApiEverest/AppServices/Validators/CustomerCreateValidator.cs
@@ -30,8 +30,8 @@ RuleFor(customer => customer)
 
             RuleFor(customer => customer.DateOfBirth)
                 .NotEmpty()
-                .WithMessage("date of birth must be informed")
-                .Must(OverAgeCustomer).WithMessage("Customer must be at least 18 years old");
+                .Must(OverAgeCustomer)
+                .WithMessage("Customer must be at least 18 years old");
 
             RuleFor(customer => customer.Country)
                 .NotEmpty()
