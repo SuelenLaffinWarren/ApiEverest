128b33bf4fc2cd7f2cc4106bb0b6ecf2174fd45f
SuelenLaffinWarren <113142506+SuelenLaffinWarren@users.noreply.github.com>
2022-12-14 08:32:13 -0300

    Update ApiEverest/AppServices/Profile/CustomerProfile.cs
    Co-authored-by: Claudinei Santos <103595662+santosclaudinei-warren@users.noreply.github.com>

--- ApiEverest/AppServices/Profile/CustomerProfile.cs
+++ ApiEverest/AppServices/Profile/CustomerProfile.cs
@@ -11,7 +11,6 @@ namespace ApiEverest.Mappers
         {
             CreateMap<CustomerEntity, CustomerCreateDto>();
             CreateMap<CustomerEntity, CustomerUpdateDto>();
-
         }
 
     }
