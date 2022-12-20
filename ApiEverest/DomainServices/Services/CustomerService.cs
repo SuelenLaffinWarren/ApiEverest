dcc047e134db7b5a62e36c4c22faa664395a3c47
SuelenLaffinWarren <113142506+SuelenLaffinWarren@users.noreply.github.com>
2022-12-20 10:44:41 -0300

    Update ApiEverest/DomainServices/Services/CustomerService.cs
    Co-authored-by: Claudinei Santos <103595662+santosclaudinei-warren@users.noreply.github.com>

--- ApiEverest/DomainServices/Services/CustomerService.cs
+++ ApiEverest/DomainServices/Services/CustomerService.cs
@@ -36,6 +36,7 @@ namespace DomainServices.Services
 
             return response;
         }
+        
         public void Update(CustomerEntity updateCustomer)
         {
             CustomerDuplicate(updateCustomer);
