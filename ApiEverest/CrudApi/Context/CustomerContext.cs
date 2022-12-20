2f43c234104c9090cc9d824ef3eaa91ac022ef79
SuelenLaffinWarren <suelen.laffin@warren.com.br>
2022-12-20 10:22:04 -0300

    fix: add yml file

---
+++ ApiEverest/CrudApi/Context/CustomerContext.cs
@@ -0,0 +1,13 @@
+﻿using DomainModels.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiEverest.Context
+{
+    public class CustomerContext : DbContext
+    {
+        public CustomerContext(DbContextOptions<CustomerContext> options) 
+            : base(options) => Database.EnsureCreated();
+        
+        public DbSet<CustomerEntity> CustomerEntity { get; set; }  
+    }
+}
--- ApiEverest/CrudApi/Program.cs
+++ ApiEverest/CrudApi/Program.cs
@@ -1,8 +1,10 @@
+using ApiEverest.Context;
 using AppServices.Services;
 using DomainServices.Services;
 using FluentValidation;
 using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System.Reflection;
@@ -17,6 +19,11 @@ var builder = WebApplication.CreateBuilder(args);
         builder.Services.AddTransient<ICustomerAppService, CustomerAppService>();
         builder.Services.AddValidatorsFromAssembly(Assembly.Load(nameof(AppServices)));
         builder.Services.AddAutoMapper(Assembly.Load(nameof(AppServices)));
+
+        builder.Services.AddDbContext<CustomerContext>
+            (options => options.UseMySql(
+                "server=localhost;initial catalog=CUSTOMER_DATABASE;uid=root;pwd=admin",
+                Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.31-mysql")));
      
         var app = builder.Build();
 
